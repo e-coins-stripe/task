0127669857b50413f11255efbfd9a15fd365fb15
taskmanager <vedantpro36@gmail.com>
2025-11-29 16:10:26 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyAsMxamUDdtbYbse359_wJJvU7smEMC7YI",
+  authDomain: "task-6a033.firebaseapp.com",
+  projectId: "task-6a033",
+  storageBucket: "task-6a033.firebasestorage.app",
+  messagingSenderId: "1058933867414",
+  appId: "1:1058933867414:web:9a8bde3d596760b39013b7"
 };
 
 firebase.initializeApp(firebaseConfig);
